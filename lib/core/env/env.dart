a9defffcd005ccb0dc64972827231a682eaadd4c
Jose Diaz <josediazsteer.io@Joses-MacBook-Pro.local>
2026-05-02 21:48:56 -0400

    chore: 1.0.0.+1 for first TestFlight

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -3,8 +3,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Env {
   const Env._();
 
-  static String get supabaseUrl => _required('SUPABASE_URL');
-  static String get supabaseAnonKey => _required('SUPABASE_ANON_KEY');
+  static String get supabaseUrl => _required('https://zragtwqxezkxbtwvriwg.supabase.co');
+  static String get supabaseAnonKey => _required('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyYWd0d3F4ZXpreGJ0d3ZyaXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NDU5NTQsImV4cCI6MjA5MzIyMTk1NH0.Web2i1pXhhn9Sk1e17efgrHfZQBaXa6lRVeKqzJLhmM');
 
   static String _required(String key) {
     final value = dotenv.maybeGet(key);
